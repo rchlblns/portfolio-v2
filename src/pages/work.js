ff4da07ec52f45f77a5c5ebcb0e27c8ed33458a6
Short Stack <41342213+rchlblns@users.noreply.github.com>
2020-09-09 14:57:31 -0500

    cards created for each project

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,16 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Work = () => (
+const Work = ({ data }) => (
+
   <Layout>
     <SEO title="Work" />
     <h1>My Work!</h1>
     <p>I built these things!</p>
+
+    {data.allProjectInfoJson.edges.map(edge =>
+      <div class="card">
+        <h2>{edge.node.name}</h2>
+        <p>Built with: {edge.node.stack}</p>
+        <p>{edge.node.description}</p>
+      </div>
+    )}
     {/* <Link to="/">Go back to the homepage</Link> */}
   </Layout>
 )
 
 export default Work
+
+export const query = graphql`
+  query ProjectsQuery {
+    allProjectInfoJson {
+      edges {
+        node {
+          name
+          stack
+          description
+          links {
+            demo
+            github
+          }
+          id
+        }
+      }
+    }
+  }
+`
