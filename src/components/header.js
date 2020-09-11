b19b89931739857930b05e3555641ea84e984693
Short Stack <41342213+rchlblns@users.noreply.github.com>
2020-09-11 13:40:28 -0500

    reorganized nav bar links, added content to about me/index page

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
+// const ListLink = props => (
+//   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+//     <Link to={props.to}>{props.children}</Link>
+//   </li>
+// )
 
 const Header = ({ siteTitle }) => (
   <div style={{ margin: `3rem auto`}}>
@@ -14,10 +14,13 @@ const Header = ({ siteTitle }) => (
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
         <h3 style={{ display: `inline` }}>{siteTitle}</h3>
       </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/work/">Work</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        {/* <ListLink to="/contact/">Contact</ListLink> */}
+      <ul style={{ listStyle: `none`, float: `right`, display: `inline-block`}}>
+        {/* <ListLink to="/work">Work</ListLink>
+        <ListLink to="/about/">Github</ListLink>
+        <ListLink to="/contact/">LinkedIn</ListLink> */}
+        <Link to="/work" style={{marginRight: `1rem`}}>Work</Link>
+        <a href="https://github.com/rchlblns" target="_blank" rel="noopener noreferrer" style={{marginRight: `1rem`}}>Github</a>
+        <a href="https://linkedin.com/in/richellebillones" target="_blank" rel="noopener noreferrer" >LinkedIn</a>
       </ul>
     </header>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi hello</h1>
-    <p>I'm Richelle</p>
+    <h1>Hi! I'm Richelle, a full stack developer in Houston, TX.</h1>
+    {/* <p>I'm interested in creating simple solutions for complex problems.</p> */}
+    <p>Current stack: React, Gatsby, GraphQL, Node.js</p>
+    <p>View my resume here. And check out my work here!</p>
+    <p>Have a question or want to say hi? Email me at </p>
     {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
   </Layout>
 )
